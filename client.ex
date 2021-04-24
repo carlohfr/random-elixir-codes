53682ef382a3281d04690e615e82b0040f8449f1
Carlo <carlohenrique78@gmail.com>
2021-04-24 08:46:23 -0300

    add gen_tcp client

---
+++ client.ex
@@ -0,0 +1,54 @@
+defmodule Client do
+
+    require Logger
+    use GenServer
+
+    @ip {127, 0, 0, 1}
+    @port 10000
+
+    def send_message(pid, message) do
+        GenServer.cast(pid, {:message, message})
+    end
+
+    def start_link(_) do
+        GenServer.start(__MODULE__, %{socket: nil})
+    end
+
+    def init(state) do
+        send(self(), :connect)
+        send_message(self(), "test message")
+        {:ok, state}
+    end
+
+    def handle_info(:connect, state) do
+        Logger.info "Connecting to #{:inet.ntoa(@ip)}:#{@port}"
+
+        case :gen_tcp.connect(@ip, @port, [:binary, active: true]) do
+            {:ok, socket} ->
+            {:noreply, %{state | socket: socket}}
+            {:error, reason} ->
+            disconnect(state, reason)
+        end
+    end
+
+    def handle_info({:tcp, _, data}, state) do
+        Logger.info "Received #{data}"
+        {:noreply, state}
+    end
+
+    def handle_info({:tcp_closed, _}, state), do: {:stop, :normal, state}
+    def handle_info({:tcp_error, _}, state), do: {:stop, :normal, state}
+
+    def handle_cast({:message, message}, %{socket: socket} = state) do
+        Logger.info "Sending #{message}"
+
+        :ok = :gen_tcp.send(socket, message)
+        {:noreply, state}
+    end
+
+    def disconnect(state, reason) do
+        Logger.info "Disconnected: #{reason}"
+        {:stop, :normal, state}
+    end
+
+end
